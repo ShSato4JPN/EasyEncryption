28379bc9bca5dd194e71b1e93f5f82ca962193ef
ShSato4 <ShSato4.private.1221@gmail.com>
2019-03-29 01:13:30 +0900

    完成間近・・・
    途中

--- javascript/CaesarCipher.js
+++ javascript/CaesarCipher.js
@@ -1,7 +1,35 @@
-/***  平文をシーザー暗号に変換するエンコーディング処理***/
+/***  平文をシーザー暗号に変換する処理***/
 var caesarEncoding = function(text, shift, loop) {
+    let rows = text.split(/\r\n|\r|\n/);
+    let result = "";
+    for (var row_cnt = 0;  row_cnt < rows.length;  row_cnt++) {
+        let row  = encodeURIComponent(rows[row_cnt]);
+        for (var l_cnt = 0; l_cnt < loop; l_cnt++) {
+            for (var i = 0; i < row.length; i++) {
+                result += String.fromCharCode(row.charCodeAt(i) + parseInt(shift));
+            }
+            result += result + "\n";
+            row = result;
+        }
+    }
+
+    return result;
 }
 
-/***  シーザー暗号を平文に変換するデコーディング処理***/
-var caesarEncoding = function(text, shift, loop) {
+/***  シーザー暗号を平文に変換する処理***/
+var caesarDecoding = function(text, shift, loop) {
+    let rows = text.split(/\r\n|\r|\n/);
+    let result = "";
+    for (var row_cnt = 0;  row_cnt < rows.length;  row_cnt++) {
+        let row  = encodeURIComponent(rows[row_cnt]);
+        for (var l_cnt = 0; l_cnt < loop; l_cnt++) {
+            for (var i = 0; i < row.length; i++) {
+                result += String.fromCharCode(row.charCodeAt(i) - parseInt(shift));
+            }
+            result += result + "\n";
+            row = result;
+        }
+    }
+
+    return result;
 }
--- javascript/EE_script.js
+++ javascript/EE_script.js
@@ -1,75 +1,96 @@
 /*** EasyEncryptionのロジック ***/
 window.onload = function() {
-    // アップロードファイル
-    var uploadFile;
     // ファイル選択フォーム
-    let input_form = document.getElementById("w-c-h-file-input-form");
+    var input_form = document.getElementById("w-c-h-file-input-form");
     // ファイルのパス
-    let txt_file = document.getElementById("w-c-h-text-file");
+    var txt_file = document.getElementById("w-c-h-text-file");
     // シフト数
-    let txt_shift = document.getElementById("w-c-h-text-shift");
+    var txt_shift = document.getElementById("w-c-h-text-shift");
     // ループ数
-    let txt_loop = document.getElementById("w-c-h-text-loop");
+    var txt_loop = document.getElementById("w-c-h-text-loop");
     // 結果出力
-    let txt_result = document.getElementById("w-c-b-textarea");
+    var txt_result = document.getElementById("w-c-b-textarea");
     // ダイアログボタン
-    let dialogBtn = document.getElementById("w-c-h-label");
+    var btn_dialog = document.getElementById("w-c-h-label");
     // 実行ボタン
-    let runBtn = document.getElementById("w-c-h-bf-label");
+    var btn_run = document.getElementById("w-c-h-bf-label");
+    // エンコードボタン
+    var rad_encode = document.getElementById("w-c-h-rad-encode")
+    //  デコードボタン;
+    var rad_decode = document.getElementById("w-c-h-rad-decode");
+    // アップロードファイル
+    let uploadFile;
+    // ファイル読み込みクラス
+    let reader = new FileReader();
 
-    // Event : ファイル選択ボタン
-    dialogBtn.addEventListener("change",  function(event) {
-        uploadFile = event.target.files;
-        txt_file.value = uploadFile[0].name;
+    // Event : ファイル読み込み
+    reader.addEventListener("load", function() {
+        txt_result.value = reader.result;
     });
 
-    // Event : ドラッグアンドドロップ
-    // drag
+    // Event : ファイル選択ボタン
+    btn_dialog.addEventListener("change",  function(event) {
+        uploadFile = event.target.files;
+        txt_file.value = uploadFile[0].name;
+
+        reader.readAsText( uploadFile[0]);
+    });
+
+    // Event : drag
     input_form.addEventListener("dragover", function(event) {
         finishBabbling(event);
         event.dataTransfer.dropEffect="copy";
     }, false);
-    // drop
+    // Event : drop
     input_form.addEventListener("drop", function(event) {
         finishBabbling(event);
-        uploadFile = event.dataTransfer.files;
+        uploadFile = event.target.files;
         txt_file.value = uploadFile[0].name;
+
+        reader.readAsText( uploadFile[0]);
     }, false);
 
     // Event : 処理を実行するボタン
-    runBtn.addEventListener("click", function(event) {
-        if (isNothing(txt_file.value)) {
-            alert("ファイルが未選択！");
-            finishBabbling(event);
-            return;
-        }
-
-        if (isNothing(txt_shift.value)) {
-            alert("SHIFTが未選択！");
-            finishBabbling(event);
-            return;
-        }
-
-        if (isNothing(txt_loop.value)) {
-            alert("LOOPが未選択！");
-            finishBabbling(event);
-            return;
-
-            let errMsg;
-            try {
-                var res = run(uploadFile);
-                txt_result.value = res;
-                errMsg = "処理終了！"
-            } catch(error) {
-                errMsg = "処理中にエラーが発生しました・・・。";
-            } finally {
-                alert(errMsg);
+    btn_run.addEventListener("click", function(event) {
+        if (validCheck(event)) {
+            if (window.confirm("実行しますか")) {
+                if (rad_encode.checked)  run(uploadFile[0], txt_shift.value, txt_loop.value, "ENCODE");
+                if (rad_decode.checked)  run(uploadFile[0], txt_shift.value, txt_loop.value, "DECODE");
             }
-
-            return;
+            // labelとbuttonのイベントが呼ばれるので一回目でバブリングを中止する
+            finishBabbling(event);
         }
     });
 }
 
-var run = function(textFile) {
+var validCheck = function (event) {
+    if (isNothing(document.getElementById("w-c-h-text-file").value)) {
+        alert("ファイルが未選択！");
+        finishBabbling(event);
+        return false;
+    }
+
+    if (isNothing(document.getElementById("w-c-h-text-shift").value)) {
+        alert("SHIFTが未選択！");
+        finishBabbling(event);
+        return false;
+    }
+
+    if (isNothing(document.getElementById("w-c-h-text-loop").value)) {
+        alert("LOOPが未選択！");
+        finishBabbling(event);
+        return false;
+    }
+
+    return true;
+}
+
+var run = function(textFile, shift, loop, mode) {
+    let result;
+    let val = document.getElementById("w-c-b-textarea").value;
+
+    if (mode === "ENCODE") result = caesarEncoding(val, shift, loop);
+    if (mode === "DECODE") result = caesarDecoding(val, shift, loop);
+
+    document.getElementById("w-c-b-textarea").value = result;
 }
